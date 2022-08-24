3e5242b321daba59fed09d44285caafe2447a24c
OlexxandrS <94759574+OlexxandrS@users.noreply.github.com>
2022-08-24 22:12:26 +0300

    addTask-done

--- todo_App/src/data/stores/useToDoStore.ts
+++ todo_App/src/data/stores/useToDoStore.ts
@@ -1,5 +1,5 @@
 import create from "zustand";
-
+import { generateId } from "../helpers";
 interface Task {
   id: string;
   title: string;
@@ -7,8 +7,44 @@ interface Task {
 }
 
 interface ToDoStore {
-  task: Task[];
+  tasks: Task[];
   createTask: (title: string) => void;
   updateTask: (id: string, title: string) => void;
   removeTask: (id: string) => void;
 }
+
+export const useToDoStore = create<ToDoStore>((set, get) => ({
+  tasks: [
+    {
+      id: "1",
+      title: "Default Task",
+      createdAt: 123456,
+    },
+  ],
+  createTask: (title) => {
+    const { tasks } = get();
+    const newTask = {
+      id: generateId(),
+      title,
+      createdAt: Date.now(),
+    };
+    set({
+      tasks: [newTask].concat(tasks),
+    });
+  },
+  updateTask: (id: string, title: string) => {
+    const { tasks } = get();
+    set({
+      tasks: tasks.map((task) => ({
+        ...task,
+        title: task.id === id ? title : task.title,
+      })),
+    });
+  },
+  removeTask: (id: string) => {
+    const { tasks } = get();
+    set({
+      tasks: tasks.filter((task) => task.id !== id),
+    });
+  },
+}));
--- todo_App/src/views/App/index.tsx
+++ todo_App/src/views/App/index.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import styles from "./index.module.scss";
+import { useToDoStore } from "../../data/stores/useToDoStore";
+import { InputPlus } from "../components/InputPlus";
 
 export const App: React.FC = () => {
+  const [tasks, createTask, updateTask, removeTask] = useToDoStore((state) => [
+    state.tasks,
+    state.createTask,
+    state.updateTask,
+    state.removeTask,
+  ]);
+
   return (
     <article className={styles.article}>
       <h1 className={styles.articleTitle}>To Do App</h1>
       <section className={styles.articleSection}></section>
+      <InputPlus
+        onAdd={(title) => {
+          if (title) createTask(title);
+        }}
+      />
       <section className={styles.articleSection}></section>
     </article>
   );
